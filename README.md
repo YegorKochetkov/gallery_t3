6b0d4d34de8f0ad8abe90c1493443a92e79fcd04
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-04-29 16:09:42 +0300

    feat: add list of mock images

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## TODO
 
-- [ ] Deploy to Vercel
-- [ ] Add basic UI with mock data
+- [x] Deploy to Vercel
+- [x] Add basic UI with mock data
 - [ ] Set up database with Vercel Postgres
 - [ ] Attach database to UI
 - [ ] Add authentication with Clerk
-- [ ] Add image upload
+- [ ] Add image upload with Uploadthing
 - [ ] Error management with Sentry
 - [ ] Parallel routing for image page
 - [ ] Server actions for delete
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import { mockImages } from "~/lib/mock-data";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
-      <h1>Hello (gallery in progress...)</h1>
+    <main className="min-h-screen bg-gray-200 p-4">
+      <ul data-images-list className="flex flex-wrap items-center justify-center gap-4">
+        {mockImages.map((image) => (
+          <li
+            key={image.id}
+            className="relative rounded w-full sm:w-48 aspect-square overflow-hidden"
+          >
+            <Image
+              src={image.url}
+              alt="random AI generated image"
+              width={500}
+              height={500}
+              className="object-cover"
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
