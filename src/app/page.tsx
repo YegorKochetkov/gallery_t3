25ed5c2e46b5b0062061b964a4c06dc36e7bef6f
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-04-29 23:58:31 +0300

    feat: set HomePage force dynamic option + vscode TS version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import { mockImages } from "~/lib/mock-data";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.debug('🚀 ~ file: page.tsx:7 ~ posts:', posts);
+
   return (
     <main className="p-4">
       <ul data-images-list className="grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))] gap-4">
