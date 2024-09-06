9f077857586cc88340f6212371d69c4611be7835
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-09-06 18:45:40 +0300

    build: update drizzle package

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,11 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.POSTGRES_URL,
-  },
-  tablesFilter: ["gallery_t3_*"],
+	schema: "./src/server/db/schema.ts",
+	// driver: "pg",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.POSTGRES_URL,
+	},
+	tablesFilter: ["gallery_t3_*"],
 } satisfies Config;
