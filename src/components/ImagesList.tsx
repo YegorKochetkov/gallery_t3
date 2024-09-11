82abeb89033e5c8fb7fcbf870ad84b152e743e72
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-09-11 18:32:31 +0300

    feat: Add user auth and filter images by user ID

--- src/components/ImagesList.tsx
+++ src/components/ImagesList.tsx
@@ -1,8 +1,15 @@
 import { db } from "~/server/db";
 import { ImageItem } from "./ImageItem";
 
+import { auth } from "@clerk/nextjs/server";
+
 export const ImagesList = async () => {
+	const user = auth();
+
+	if (!user.userId) return null;
+
 	const images = await db.query.images.findMany({
+		where: (model, { eq }) => eq(model.userId, user.userId),
 		orderBy: (model, { asc }) => asc(model.name),
 	});
 
