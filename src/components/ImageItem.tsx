6ec9fdeeff79fc2155861deab8cb15da901b9368
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-09-19 22:07:24 +0300

    refactor: Refactored ImageItem component for hover effect styling.

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export const ImageItem = (image: { url: string; name: string }) => {
 	return (
-		<figure>
-			<div className="shimmer relative overflow-clip rounded bg-gray-300">
+		<figure className="group">
+			<div className="relative bg-gray-300 rounded overflow-clip shimmer">
 				<Image
 					src={image.url}
 					alt={image.name}
 					width={200}
 					height={200}
-					className="w-full transform-gpu object-cover transition-transform hover:scale-105"
+					className="group-hover:scale-105 w-full transform-gpu transition-transform object-cover"
 				/>
 			</div>
 			<figcaption>{image.name}</figcaption>
