556e041dd89dd82f477b7dd48f2203f53bc109fb
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-10-28 22:42:18 +0200

    feat: Make header sticky

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -10,7 +10,7 @@ export const TopNav = () => {
 	const router = useRouter();
 
 	return (
-		<header data-main-header className="border-gray-400 border-b">
+		<header data-main-header className="top-0 z-10 sticky border-gray-400 bg-gray-200 bg-opacity-30 backdrop-blur-2xl border-b">
 			<h1 className="sr-only">Gallery T3 App</h1>
 			<nav className="flex justify-between items-center p-4 w-full font-semibold text-xl">
 				<span>TopNav</span>
