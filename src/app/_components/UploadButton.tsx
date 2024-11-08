9152f765f40d2deed0275f7f591ccb118c94632b
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-11-08 13:26:35 +0200

    refactor: Replace custom upload button with shadcn ui button

--- src/app/_components/UploadButton.tsx
+++ src/app/_components/UploadButton.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { Upload, ClockArrowUp } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 import { useUploadThing } from "~/lib/uploadthing";
+import { buttonVariants } from '~/components/ui/button';
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -30,21 +32,6 @@ const useUploadThingInputProps = (...args: Input) => {
   };
 };
 
-const UploadButtonSVG = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
-    </svg>
-  );
-}
-
-const IsUploadingSVG = () => {
-  return (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-  </svg>
-  );
-}
-
 export const UploadButton = () => {
   const router = useRouter();
   const { inputProps, isUploading } = useUploadThingInputProps("imageUploader", {
@@ -57,9 +44,9 @@ export const UploadButton = () => {
     <div>
       <label
         htmlFor="upload-button"
-        className={`cursor-pointer ${isUploading ? "pointer-events-none cursor-wait" : ""}`}
+        className={`cursor-pointer ${isUploading ? "pointer-events-none cursor-wait" : ""} ${buttonVariants({ variant: "outline" })}`}
       >
-        {isUploading ? <IsUploadingSVG /> : <UploadButtonSVG />}
+        {isUploading ? <ClockArrowUp /> : <Upload />}
       </label>
       <input
         id="upload-button"
