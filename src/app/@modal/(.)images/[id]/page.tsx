3a7b279f7fa4c1d875f7bfc2c6682acad5c5279d
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-11-14 18:59:27 +0200

    fix: Update ImageModal to accept Promise for id and add isInModalView.

--- src/app/@modal/(.)images/[id]/page.tsx
+++ src/app/@modal/(.)images/[id]/page.tsx
@@ -1,16 +1,9 @@
 import { ImageFullPageView } from "~/components/ImageFullPageView";
 import { Modal } from "~/components/Modal";
 
-export default async function ImageModal({
-  params: { id: imageId }
-}: {
-  params: { id: string }
-}) {
-  const idAsNumber = Number(imageId);
-
-  if (Number.isNaN(idAsNumber)) {
-    return <p className="absolute inset-0 text-2xl text-center">Invalid image ID</p>;
-  }
+export default async function ImageModal(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+  const isInModalView = true;
 
   return (
     <Modal>
